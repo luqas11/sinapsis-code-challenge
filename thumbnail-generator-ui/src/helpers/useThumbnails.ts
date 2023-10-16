7f51a5c1e432d8b70b3d0b9a0af35edcaed7d07c
luqas11 <lucascazas@hotmail.com>
2023-10-15 22:15:14 -0300

    Add mock to make the app work without json-server

--- thumbnail-generator-ui/src/helpers/useThumbnails.ts
+++ thumbnail-generator-ui/src/helpers/useThumbnails.ts
@@ -5,13 +5,38 @@ import axios from "axios";
  * Hook to encapsulate server state logic through a Tanstack Query mutation.
  */
 const useThumbnails = () => {
+  const mockValues = [
+    {
+      label: "120x120",
+      url: "../../../mock_images/120x120.png",
+    },
+    {
+      label: "160x120",
+      url: "../../../mock_images/160x120.png",
+    },
+    {
+      label: "400x300",
+      url: "../../../mock_images/400x300.png",
+    },
+  ];
+  const mockDelay = 2000;
+
   return useMutation({
     mutationKey: ["thumbnails"],
     mutationFn: async (selectedImage: string) => {
-      const response = await axios.get("http://localhost:3000/get-thumbnails", {
-        data: { image: selectedImage },
-      });
-      return response.data;
+      const mockBackend = import.meta.env.VITE_MOCK_BE;
+      if (mockBackend) {
+        await new Promise((r) => setTimeout(r, mockDelay));
+        return mockValues;
+      } else {
+        const response = await axios.get(
+          "http://localhost:3000/get-thumbnails",
+          {
+            data: { image: selectedImage },
+          }
+        );
+        return response.data;
+      }
     },
   });
 };
